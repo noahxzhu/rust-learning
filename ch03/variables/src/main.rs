e8a0eea8a658b1469e6cf7bec577c501270b12cb
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-02 01:29:03 +0800

    Add variables

---
+++ ch03/variables/src/main.rs
@@ -0,0 +1,23 @@
+// Constants
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    // let mut x = 5;
+    // println!("This value of x is: {x}");
+    // x = 6;
+    // println!("This value of x is: {x}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    // let spaces = "    ";
+    // let spaces = spaces.len();
+}
