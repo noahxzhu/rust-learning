3e9725679f6d81561ef3fdec255838627cf0b155
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-05 15:03:54 +0800

    Add defining_and_instantiating_structs

---
+++ TRPL/ch05/defining_and_instantiating_structs/src/main.rs
@@ -0,0 +1,45 @@
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
+
+fn main() {
+    // let user1 = User {
+    //     active: true,
+    //     username: String::from("someusername123"),
+    //     email: String::from("someone@example.com"),
+    //     sign_in_count: 1,
+    // };
+
+    // let mut user1 = User {
+    //     active: true,
+    //     username: String::from("someusername123"),
+    //     email: String::from("someone@example.com"),
+    //     sign_in_count: 1,
+    // };
+
+    // user1.email = String::from("anotheremail@exmaple.com");
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+    //
+    // let user3 = User {
+    //     email: String::from("another@example.com"),
+    //     ..user1
+    // };
+}
+
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         active: true,
+//         username,
+//         email,
+//         sign_in_count: 1,
+//     }
+// }
