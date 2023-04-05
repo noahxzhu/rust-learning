f97f53ae267d1ab3108050412566bad85e836e80
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-05 20:22:44 +0800

    Add match_control

---
+++ TRPL/ch06/match_control/src/main.rs
@@ -0,0 +1,38 @@
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+// }
+//
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+//
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}", state);
+//             25
+//         }
+//     }
+// }
+
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         Some(i) => Some(i + 1),
+//         None => None,
+//     }
+// }
+
+fn main() {
+    // let five = Some(5);
+    // let six = plus_one(five);
+    // let none = plus_one(None);
+    println!("Hello, world!");
+}
