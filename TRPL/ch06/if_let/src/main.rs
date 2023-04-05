c831f405e78d74d91a232008aa33dafe07310bd3
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-05 20:43:41 +0800

    Add if_let

---
+++ TRPL/ch06/if_let/src/main.rs
@@ -0,0 +1,38 @@
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+// }
+//
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+
+fn main() {
+    // let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (),
+    // }
+
+    // let config_max = Some(3u8);
+    // if let Some(max) = config_max {
+    //     println!("The maximum is configured to be {}", max);
+    // }
+
+    // let mut count = 1;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}", state),
+    //     _ => count += 1,
+    // }
+
+    // let mut count = 1;
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State quarter from {:?}", state);
+    // } else {
+    //     count += 1;
+    // }
+}
