89b0ec65e107f2038b9e953609e5bfec00ad8efc
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-08 02:04:33 +0800

    Add aggregator

---
+++ TRPL/ch10/aggregator/src/lib.rs
@@ -0,0 +1,57 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    // fn summarize(&self) -> String {
+    //     format!("{}: {}", self.username, self.content)
+    // }
+}
+
+// pub fn notify<T: Summary>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// pub fn notify<T: Summary + Display>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify(item: &(impl Summary + Display)) {
+//     println!("Breaking news! {}", item.summarize());
+// }
