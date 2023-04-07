3e934afda52ebdc4f481e1ff596cd021aab415fd
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-07 14:58:39 +0800

    Add hashmaps

---
+++ TRPL/ch08/hashmaps/src/main.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    // let team_name = String::from("Blue");
+    // let score = scores.get(&team_name).copied().unwrap_or(0);
+    //
+    // println!("{score}");
+    //
+    // for (key, value) in scores {
+    //     println!("{key}: {value}");
+    // }
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Blue")).or_insert(40);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
