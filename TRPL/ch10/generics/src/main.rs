28860440121212c3e07fd8098de73dcb350e4185
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-07 23:25:03 +0800

    Add generics

---
+++ TRPL/ch10/generics/src/main.rs
@@ -0,0 +1,22 @@
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![10021, 123, 131, 23757, 101];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
