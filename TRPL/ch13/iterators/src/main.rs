8a6a273e2c473f305c8119b28acd919c50c28947
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-11 13:53:21 +0800

    Add iterators

---
+++ TRPL/ch13/iterators/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // let v1 = vec![1,2,3];
+    //
+    // let v1_iter = v1.iter();
+    //
+    // for val in v1_iter {
+    //     println!("Got: {}", val);
+    // }
+    //
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
