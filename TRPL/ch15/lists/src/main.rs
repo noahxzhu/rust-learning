a9ab55a5bcb49c5ad61b3d083b19c7ea1368b69f
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-12 18:23:49 +0800

    Add lists

---
+++ TRPL/ch15/lists/src/main.rs
@@ -0,0 +1,33 @@
+// enum List {
+//     Cons(i32, Box<List>),
+//     Nil,
+// }
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use std::rc::Rc;
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // let b = Cons(3, Rc::clone(&a));
+    // let c = Cons(4, Rc::clone(&a));
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+}
