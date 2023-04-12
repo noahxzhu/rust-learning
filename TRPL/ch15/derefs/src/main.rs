e890bb6794c244327f2b774423fad014a6d7d19c
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-12 15:12:56 +0800

    Add derefs

---
+++ TRPL/ch15/derefs/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // let x = 5;
+    // let y = &x;
+    //
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
