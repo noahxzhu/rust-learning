48a2deb51e6b67e5d7bd672089ed4c9eff3b6d32
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-15 00:46:44 +0800

    Add thread pool for hello

--- TRPL/ch20/hello/src/main.rs
+++ TRPL/ch20/hello/src/main.rs
@@ -2,14 +2,25 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
@@ -49,11 +60,28 @@ fn handle_connection(mut stream: TcpStream) {
     //     stream.write_all(response.as_bytes()).unwrap();
     // }
 
+    // let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+    //     ("HTTP/1.1 200 OK", "hello.html")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // };
+    //
+    // let contents = fs::read_to_string(filename).unwrap();
+    // let length = contents.len();
+    //
+    // let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    //
+    // stream.write_all(response.as_bytes()).unwrap();
+
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
