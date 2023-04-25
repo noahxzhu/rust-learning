437c8471c8412abe668295537adbd5d0cbb762a5
Noah Zhu <noah.x.zhu@gmail.com>
2023-04-25 20:02:44 +0800

    Add say-world

--- TOKIO/my-redis/src/main.rs
+++ TOKIO/my-redis/src/main.rs
@@ -1,14 +1,58 @@
-use mini_redis::{client, Result};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    client.set("hello", "world".into()).await?;
+    println!("Listening");
 
-    let result = client.get("hello").await?;
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
-    println!("got value from the server; result={:?}", result);
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
 
-    Ok(())
+        let db = db.clone();
+
+        println!("Accepted");
+
+        tokio::spawn(async move {
+            process(socket, db).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream, db: Db) {
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&response).await.unwrap();
+    }
 }
---
+++ TOKIO/say-world/src/main.rs
@@ -0,0 +1,11 @@
+async fn say_world() {
+    println!("world");
+}
+
+#[tokio::main]
+async fn main() {
+    let op = say_world();
+
+    println!("hello");
+    op.await;
+}
