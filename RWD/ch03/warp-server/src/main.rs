4b0782094b36fb01aa01349570970453c996d7f5
Noah Zhu <noah.x.zhu@gmail.com>
2023-05-03 22:45:26 +0800

    Add ch03

---
+++ RWD/ch03/warp-server/src/main.rs
@@ -0,0 +1,8 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path("hello").map(|| format!("Hello, World!"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+}
